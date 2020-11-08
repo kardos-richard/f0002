a56e7da90bef09c14adf3ba1963d32a4b0043a2c
kardos-richard <55028258+kardos-richard@users.noreply.github.com>
2020-11-08 14:03:31 +0100

    Create f0002b.py

---
+++ f0002b.py
@@ -0,0 +1,12 @@
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
+
+A program lefuttatása után az utolsó dolog amit kifog írni : cica
